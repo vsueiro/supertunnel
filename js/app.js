2ac8605ec479d2b17cfc975d36947c4fa0ae2967
Vinicius Sueiro <hi@vsueiro.com>
2021-06-24 14:18:27 -0300

    Prints values to help calibrate angle handling (gamma and portrait/landscape)

--- js/app.js
+++ js/app.js
@@ -865,28 +865,22 @@ let app = {
 
     handle : function( event ) {
 
-      // Implements world-based calibration on iOS (alpha is 0 when pointing North)
+      // Implements world-based calibration on iOS (alpha is 0 when pointing North), based on:      
       // https://www.w3.org/2008/geolocation/wiki/images/e/e0/Device_Orientation_%27alpha%27_Calibration-_Implementation_Status_and_Challenges.pdf
 
-      console.log( 'orientationControl: ', app.options.orientationControl );
-      console.log( 'event.absolute: ', event.absolute );
-      console.log( 'event.webkitCompassAccuracy: ', event.webkitCompassAccuracy );
-      console.log( 'event.webkitCompassHeading: ', event.webkitCompassHeading );
-      console.log( 'event.alpha: ', event.alpha );
-      console.log( 'app.data.orientation.initialOffset: ', app.data.orientation.initialOffset );
+      document.querySelector( 'pre' ).textContent = event.alpha + '\n' + event.beta + '\n' + event.gamma;
 
       if ( app.data.orientation.initialOffset === undefined && event.absolute !== true )
         app.data.orientation.initialOffset = event.webkitCompassHeading || 0;
 
       let alpha = event.alpha;
 
+      // Calibrates alpha to make it North-based
       if ( event.absolute !== true )
         alpha = alpha - app.data.orientation.initialOffset;
 
       if ( alpha < 0 )
         alpha +=360;
-
-      console.log( 'alpha: ', alpha );
 
       // Updates values to be used on render function
       app.data.orientation.alpha = alpha;
